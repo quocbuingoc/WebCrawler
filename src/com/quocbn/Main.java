4e71e8c4d239e1b81503f1546b12fc4b894d88d4
quocb <quocb@BNQ_PC>
2020-05-21 23:09:41 +0700

    add new crawler to new branch

--- src/com/quocbn/Main.java
+++ src/com/quocbn/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new BasicWebCrawler().getPageLinks("http://www.google.com/");
+//		new BasicWebCrawler().getPageLinks("http://www.google.com/");
+//		new AdvangeWebCralwer().getPageLinks("https://www.google.com/", 0);
+		Extraction bwc = new Extraction();
+		bwc.getPageLinks("http://www.mkyong.com");
+		bwc.getArticles();
+		bwc.writeToFile("Java 8 Articles");
 	}
 }
