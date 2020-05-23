bc283b1341a0591a7968701fc23717e8bd54349a
levanquoc <levanquocbkdn@gmail.com>
2020-05-23 07:55:47 +0000

    Add helloworld.java file

---
+++ src/com/quocbn/helloworld.java
@@ -0,0 +1,9 @@
+#Quoc oi co len 
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Quoc oi co len,hoc de co cuoc song tot hon ");
+    }
+
+}
