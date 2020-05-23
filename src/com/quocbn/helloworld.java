5479eb2aa9372cbc12056f3399a31458ae8e05c0
levanquoc <levanquocbkdn@gmail.com>
2020-05-23 08:34:03 +0000

    remove helloworld.java file

--- src/com/quocbn/helloworld.java
+++
@@ -1,9 +0,0 @@
-#Quoc oi co len 
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        
-        System.out.println("Quoc oi co len,hoc de co cuoc song tot hon ");
-    }
-
-}
